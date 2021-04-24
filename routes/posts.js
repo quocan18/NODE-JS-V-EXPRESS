4587c8fb6abd36488cc3c0167804b22b31fb684c
user.name like: "quocan18 <user.email like: "ankyunguyen2000@gmail.com>
2021-04-24 22:22:33 +0700

    ok

--- routes/posts.js
+++ routes/posts.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 const Post = require("../models/Post");
 // Routes
+// async/await là một cơ chế giúp bạn thực hiện
+//các thao tác bất đồng bộ một cách tuần tự hơn
+// async: làm ra một function trả về một promise
+// await: làm ra một function đợi một promise
 
 // Get all the post
 router.get("/", async (req, res) => {
@@ -60,4 +64,65 @@ router.patch("/:postId", async (req, res) => {
   }
 });
 
+// JS Promise: bao gồm producing code và gọi đến consuming code
+let my_promise = new Promise(function (my_resolve, my_reject) {
+  // Producing code
+  my_resolve(); // Thành công
+  my_reject(); // Lỗi
+});
+
+// Consuming code
+my_promise.then(
+  function (value) {
+    // code successful
+  },
+  function (error) {
+    // code failed
+  }
+);
+
+//### Promise example
+const my_promise = new Promise(function (my_resolve, my_reject) {
+  const req = new XMLHttpRequest();
+  req.open("GET", "my_car.html");
+  req.onload = function (params) {
+    if (req.status == 200) {
+      my_resolve(req.response);
+    } else {
+      my_reject("Không tìm thấy file");
+    }
+  };
+  req.send();
+});
+
+my_promise.then(
+  function (value) {
+    {
+      value;
+    }
+  },
+  function (error) {
+    {
+      error;
+    }
+  }
+);
+
+// async, await và promise
+async function get_file() {
+  let my_promise = new Promise(function (my_resolve, my_reject) {
+    let req = new XMLHttpRequest();
+    req.open("GET", "my_car.html");
+    req.onload = function () {
+      if (req.status == 200) {
+        my_resolve(req.response);
+      } else {
+        my_resolve("File error");
+      }
+    };
+    req.send();
+  });
+  document.getElementById("demo").innerHTML = await my_promise;
+}
+get_file();
 module.exports = router;
